60a3db5f3f138784fb9e9b8d66b64eb53ba5ca9f
jinhankim <jinhan38@naver.com>
2025-01-15 16:46:46 +0900

    text, image widget

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -5,8 +5,11 @@ import 'package:online_lecture_smwu/screen/column_row_advanced_screen.dart';
 import 'package:online_lecture_smwu/screen/column_screen.dart';
 import 'package:online_lecture_smwu/screen/container_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/container_screen.dart';
+import 'package:online_lecture_smwu/screen/image/image_screen.dart';
 import 'package:online_lecture_smwu/screen/row/row_practice_screen.dart';
 import 'package:online_lecture_smwu/screen/row/row_screen.dart';
+import 'package:online_lecture_smwu/screen/text/text_practice_screen.dart';
+import 'package:online_lecture_smwu/screen/text/text_screen.dart';
 
 class ButtonScreen extends StatelessWidget {
   const ButtonScreen({super.key});
@@ -90,6 +93,36 @@ class ButtonScreen extends StatelessWidget {
                   ));
                 },
                 child: const Text("Column, Row 심화")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const TextScreen();
+                    },
+                  ));
+                },
+                child: const Text("Text")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const TextPracticeScreen();
+                    },
+                  ));
+                },
+                child: const Text("Text 실습")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ImageScreen();
+                    },
+                  ));
+                },
+                child: const Text("Image")),
           ],
         ),
       ),
