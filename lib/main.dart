cf08bb068d288d334361bb183eb085d5cb26bb8e
jinhankim <jinhan38@naver.com>
2025-01-15 15:21:23 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:online_lecture_smwu/screen/button_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ButtonScreen(),
+    );
+  }
+}
+
---
+++ lib/screen/button_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:online_lecture_smwu/screen/column_practice_screen.dart';
+import 'package:online_lecture_smwu/screen/column_row_advanced_screen.dart';
+import 'package:online_lecture_smwu/screen/column_screen.dart';
+import 'package:online_lecture_smwu/screen/container_practice_screen.dart';
+import 'package:online_lecture_smwu/screen/container_screen.dart';
+import 'package:online_lecture_smwu/screen/row/row_practice_screen.dart';
+import 'package:online_lecture_smwu/screen/row/row_screen.dart';
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold 화면에 기초가 되는 위젯
+    // Column 세로로 위젯을 나열하는 위젯
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  // 화면 이동 Navigator
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ContainerScreen();
+                    },
+                  ));
+                },
+                child: const Text("Container")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ContainerPracticeScreen();
+                    },
+                  ));
+                },
+                child: const Text("Container 실습")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ColumnScreen();
+                    },
+                  ));
+                },
+                child: const Text("Column")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ColumnPracticeScreen();
+                    },
+                  ));
+                },
+                child: const Text("Column 실습")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const RowScreen();
+                    },
+                  ));
+                },
+                child: const Text("Row")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const RowPracticeScreen();
+                    },
+                  ));
+                },
+                child: const Text("Row 실습")),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ColumnRowAdvancedScreen();
+                    },
+                  ));
+                },
+                child: const Text("Column, Row 심화")),
+          ],
+        ),
+      ),
+    );
+  }
+}
