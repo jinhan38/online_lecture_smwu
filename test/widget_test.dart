82b17bd9beb98222060b4415166ed6d6e08c24a3
jinhankim <jinhan38@naver.com>
2025-01-16 14:49:31 +0900

    variable 추가

---
+++ test/widget_test.dart
@@ -0,0 +1,27 @@
+void main() {}
+
+void variableTest() {
+  String name = "김진한";
+  print(name);
+
+  double weight = 70;
+  print(weight);
+
+  int age = 34;
+  print(age);
+
+  bool b = true;
+  b = false;
+  print(b);
+
+  var name2 = "김진한";
+
+  /// dynamic을 많이 사용하지는 않음
+  dynamic name3 = "jinhan";
+  name3 = 1;
+  name3 = false;
+  print(name3);
+
+  // 상수를 final, const
+  const name5 = "휘건";
+}
