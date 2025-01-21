c9adb3a74dac151e577f9650b404f1977a092d96
jinhankim <jinhan38@naver.com>
2025-01-21 16:55:54 +0900

    network -2 실습중

--- lib/screen/network/member_register_screen.dart
+++ lib/screen/network/member_register_screen.dart
@@ -36,7 +36,7 @@ class _MemberRegisterScreenState extends State<MemberRegisterScreen> {
                 onPressed: () async {
                   /// 회원가입 API 호출
                   Dio dio = Dio(BaseOptions(
-                      baseUrl: "https://244b-110-8-126-227.ngrok-free.appㄹ"));
+                      baseUrl: "https://244b-110-8-126-227.ngrok-free.app"));
                   var response = await dio.post("/api/v1/member", data: {
                     "email": idController.text,
                     "password": pwController.text,
