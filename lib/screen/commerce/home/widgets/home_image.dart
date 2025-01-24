6c93826f67b402d02e4a8c35281b967270a54e25
jinhankim <jinhan38@naver.com>
2025-01-24 14:42:11 +0900

    home 화면 개발중 - 2

--- lib/screen/commerce/home/widgets/home_image.dart
+++ lib/screen/commerce/home/widgets/home_image.dart
@@ -18,20 +18,49 @@ class _HomeImageState extends State<HomeImage> {
     AssetsPath.cloth6,
   ];
 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 300,
-      width: double.infinity,
-      child: PageView.builder(
-        itemCount: imageList.length,
-        itemBuilder: (context, index) {
-          return Image.asset(
-            imageList[index],
-            fit: BoxFit.cover,
-          );
-        },
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 300,
+          width: double.infinity,
+          child: PageView.builder(
+            onPageChanged: (value) {
+              selectedIndex = value;
+              setState(() {});
+            },
+            itemCount: imageList.length,
+            itemBuilder: (context, index) {
+              return Image.asset(
+                imageList[index],
+                fit: BoxFit.cover,
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            imageList.length,
+            (index) {
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 400),
+                height: 10,
+                width: index == selectedIndex ? 20 : 10,
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: index == selectedIndex ? Colors.grey : Colors.grey.shade300,
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
